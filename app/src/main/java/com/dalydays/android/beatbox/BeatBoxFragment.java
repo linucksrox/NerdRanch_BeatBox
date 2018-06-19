b920fab18f067165a22f24c7ade6e0817d4bab98
linucksrox <ericdalyblog@dalydays.com>
2018-06-19 07:01:35 -0400

    Use data binding to inflate layout, page 1323

--- app/src/main/java/com/dalydays/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/dalydays/android/beatbox/BeatBoxFragment.java
@@ -1,9 +1,27 @@
 package com.dalydays.android.beatbox;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dalydays.android.beatbox.databinding.FragmentBeatBoxBinding;
 
 public class BeatBoxFragment extends Fragment {
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box, container, false);
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return binding.getRoot();
     }
 }
