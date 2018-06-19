259cfeb03626db45badf16a26f03d8065c9f5083
linucksrox <ericdalyblog@dalydays.com>
2018-06-19 13:39:15 -0400

    Test reading in assets from BeatBox, page 1341

--- app/src/main/java/com/dalydays/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/dalydays/android/beatbox/BeatBoxFragment.java
@@ -15,8 +15,18 @@ import com.dalydays.android.beatbox.databinding.FragmentBeatBoxBinding;
 import com.dalydays.android.beatbox.databinding.ListItemSoundBinding;
 
 public class BeatBoxFragment extends Fragment {
+
+    private BeatBox mBeatBox;
+
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Nullable
