19e768f06bf50f689172f5bdb53470c255026396
linucksrox <ericdalyblog@dalydays.com>
2018-06-22 06:57:39 -0400

    Load sounds in SoundPool, page 1397

--- app/src/main/java/com/dalydays/android/beatbox/Sound.java
+++ app/src/main/java/com/dalydays/android/beatbox/Sound.java
@@ -3,6 +3,7 @@ package com.dalydays.android.beatbox;
 public class Sound {
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
 
     public Sound(String assetPath) {
         mAssetPath = assetPath;
@@ -21,5 +22,13 @@ public class Sound {
 
     public void setName(String name) {
         mName = name;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
     }
 }
