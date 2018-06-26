e6b82e2dc09c963e7a03e06ba8c6e354aa5b2fac
linucksrox <ericdalyblog@dalydays.com>
2018-06-26 07:03:05 -0400

    Add first unit test for SoundViewModel, page 1411

---
+++ app/src/test/java/com/dalydays/android/beatbox/SoundViewModelTest.java
@@ -0,0 +1,12 @@
+package com.dalydays.android.beatbox;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class SoundViewModelTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
