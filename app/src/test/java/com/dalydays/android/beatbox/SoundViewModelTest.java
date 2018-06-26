9881856b2b4f340a40a98bfaaae1778f759577bd
linucksrox <ericdalyblog@dalydays.com>
2018-06-26 07:12:15 -0400

    Fix dependencies, write setup method of test, page 1418

--- app/src/test/java/com/dalydays/android/beatbox/SoundViewModelTest.java
+++ app/src/test/java/com/dalydays/android/beatbox/SoundViewModelTest.java
@@ -2,11 +2,18 @@ package com.dalydays.android.beatbox;
 
 import org.junit.Before;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 public class SoundViewModelTest {
+    private BeatBox mBeatbox;
+    private Sound mSound;
+    private SoundViewModel mSubject;
 
     @Before
     public void setUp() throws Exception {
+        mBeatbox = mock(BeatBox.class);
+        mSound = new Sound("assetPath");
+        mSubject = new SoundViewModel(mBeatbox);
+        mSubject.setSound(mSound);
     }
 }
