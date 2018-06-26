6b662e19b5916d46f23a5bae4bdde093cee5fb1c
linucksrox <ericdalyblog@dalydays.com>
2018-06-26 12:46:32 -0400

    Add test and implement onButtonClicked method, page 1430

--- app/src/test/java/com/dalydays/android/beatbox/SoundViewModelTest.java
+++ app/src/test/java/com/dalydays/android/beatbox/SoundViewModelTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class SoundViewModelTest {
     private BeatBox mBeatbox;
@@ -23,5 +24,11 @@ public class SoundViewModelTest {
     @Test
     public void exposesSoundNameAsTitle() {
         assertThat(mSubject.getTitle(), is(mSound.getName()));
+    }
+
+    @Test
+    public void callsBeatBoxPlayOnButtonClicked() {
+        mSubject.onButtonClicked();
+        verify(mBeatbox).play(mSound);
     }
 }
